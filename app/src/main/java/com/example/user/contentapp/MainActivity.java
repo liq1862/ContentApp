f27b5167e47325958b1d25194f5b133003450066
lili <liq1862@gmail.com>
2016-12-21 11:06:49 +0800

    向使用者要求權限

--- app/src/main/java/com/example/user/contentapp/MainActivity.java
+++ app/src/main/java/com/example/user/contentapp/MainActivity.java
@@ -1,29 +1,73 @@
 package com.example.user.contentapp;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import static android.Manifest.permission.READ_CONTACTS;
 import static android.Manifest.permission.WRITE_CONTACTS;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CODE = 1;
+    private static final int REQUEST_CONTACTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int permission = ActivityCompat.checkSelfPermission(this, READ_CONTACTS);
+        int permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
 
         if(permission != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[] {READ_CONTACTS, WRITE_CONTACTS},REQUEST_CODE);
+            ActivityCompat.requestPermissions(this, new String[] {READ_CONTACTS, WRITE_CONTACTS},REQUEST_CONTACTS);
         }
         else{
-            
+//            readContacts();
         }
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_CONTACTS:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    readContacts();
+                }
+                else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("使用此應用程式需允許聯絡人權限才可顯示資訊");
+                    builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {READ_CONTACTS, WRITE_CONTACTS},REQUEST_CONTACTS);
+                        }
+                    });
+                    builder.show();
+                }
+                return;
+        }
+    }
+
+    private void readContacts() {
+        ContentResolver resolver = getContentResolver();
+        Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI,null,null,null,null);
+
+        while (cursor.moveToNext()){
+            int id = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+            Log.d("RECORD",id+"/"+name);
+        }
+    }
+
+
 }
